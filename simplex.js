e05aa806333dddeeee9fe040ccbe5451c6c2e75c
Miles <milesbasnillo@gmail.com>
2016-11-27 18:51:37 +0800

    edited initial UI

--- simplex.js
+++ simplex.js
@@ -1,10 +1,31 @@
 "use strict";
 
 $(document).ready(function (){
-	var i = 0;
-	$.each(food, function(key, value){
-		console.log(value.name);
-		i++;
+
+	$("#add_const").click(function(){
+		console.log("clicked");
+		$('#const_row').add("<input type='text' class='const' placeholder='Insert here'>").appendTo("#const_row");
 	});
-	alert("There are "+i+" foods");
+
+	/*
+	$("#rem_const").click(function(){
+		$("#const_row").last().remove();
+	});
+	*/
+
+	$("#btn_min").click(function(){
+		minimize();
+	});
+	$("#btn_max").click(function(){
+
+	});
 });
+
+
+function minimize(){
+	var obj_func = $("#obj_fxn").val();
+
+	if(!obj_func){
+		alert("No objective function!");
+	}
+}
